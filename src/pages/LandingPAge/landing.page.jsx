af38d31b4751ba879177b9c73466c0676bd95a61
Ali Hamdan <ashamdan1990@gmail.com>
2021-12-29 12:57:25 +0200

    Update landing.page.jsx

--- src/pages/LandingPAge/landing.page.jsx
+++ src/pages/LandingPAge/landing.page.jsx
@@ -31,7 +31,7 @@ const LandingPage = () => {
                 <WelcomeLogoImgWrapper />
 
                 <WelcomeLogo>
-                    A&N
+                    A&H
                 </WelcomeLogo>
 
             </WelcomeLogoContainer>
